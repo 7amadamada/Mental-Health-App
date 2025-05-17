0fb173dffe27176ffe064d92e04fb29d38f29693
Binglad <bingladbusiness@gmail.com>
2025-05-17 14:57:50 +0300

    debugger

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('App component is rendering' );
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, welcome to Radiant</Text>
       <StatusBar style="auto" />
     </View>
   );
