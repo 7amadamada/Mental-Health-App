817f0e8dbdb35b90b598c3c93f3e7df5cba1d3c5
Binglad <bingladbusiness@gmail.com>
2025-05-17 19:04:08 +0300

    moved server.js to another backend folder to verify mongoDB download

---
+++ radiant-backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/radiant')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+app.get('/', (req, res) => {
+    res.send('Radiant API is running');
+    });
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
