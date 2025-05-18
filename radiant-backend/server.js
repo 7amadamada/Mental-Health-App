139583c60e14f5d02a14ae4785287ece5b12cedf
Binglad <bingladbusiness@gmail.com>
2025-05-18 13:58:10 +0300

    added mongoDB for databease for mood tracking and to use HTTPS and encrypt data using mongoose-encryption

--- radiant-backend/server.js
+++ radiant-backend/server.js
@@ -10,6 +10,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/radiant')
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Error connecting to MongoDB:', err));
 
+
 app.get('/', (req, res) => {
     res.send('Radiant API is running');
     });
