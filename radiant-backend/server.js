6b8f67630f0727ba4b6730103037d33c66b18ba9
Binglad <bingladbusiness@gmail.com>
2025-05-18 14:02:18 +0300

    defined mood schema for mood tracking

--- radiant-backend/server.js
+++ radiant-backend/server.js
@@ -10,6 +10,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/radiant')
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Error connecting to MongoDB:', err));
 
+const moodSchema = new mongoose.Schema({
+
+mood: { type: String,required: true},
+date: { type: Date, default: Date.now}
+});
+
+const mood = mongoose.model('Mood', moodSchema);
+
+
 
 app.get('/', (req, res) => {
     res.send('Radiant API is running');
