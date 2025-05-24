bdcf9d57db742cb2fae0696fe9b564b1032dd72f
Binglad <bingladbusiness@gmail.com>
2025-05-24 19:08:37 +0300

    added necessary modules

--- radiant-backend/server.js
+++ radiant-backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(morgan('dev'));
 
 mongoose.connect('mongodb://127.0.0.1:27017/radiant')
     .then(() => console.log('Connected to MongoDB'))
@@ -18,7 +22,14 @@ date: { type: Date, default: Date.now}
 
 const mood = mongoose.model('Mood', moodSchema);
 
-
+app.get('/api/moods', async (req, res) => {
+    try {
+        const moods = await mood.find();
+        res.json(moods);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Radiant API is running');
