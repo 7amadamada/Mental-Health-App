a81650f25caa8f72e4adb6ee9e9cb787f9f6f96a
Binglad <bingladbusiness@gmail.com>
2025-05-24 23:59:58 +0300

    initialized index.js, which is the central hub for all the models initialized and exports them as an object.

---
+++ radiant-backend/main models/index.js
@@ -0,0 +1,13 @@
+const User = require('./users');
+const Meditation = require('./meditation');
+const Mood = require('./mood');
+const Journal = require('./journal');
+const fitness = require('./fitness');
+
+module.exports = {
+    User,
+    Meditation,
+    Mood,
+    Journal,
+    fitness
+};
