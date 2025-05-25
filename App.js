c579489f4a642355235d631a559269820deefda9
Binglad <bingladbusiness@gmail.com>
2025-05-25 23:40:50 +0300

    added a few codes to tie between the screens and the model

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import {NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+
 import HomeScreen from './radiant-backend/app/screens/HomeScreen.js';
 import {LoginScreen} from './radiant-backend/app/screens/LoginScreen.js';
 import {SignupScreen} from './radiant-backend/app/screens/SignupScreen.js';
 import ChangePasswordScreen from './radiant-backend/app/screens/ChangePasswordScreen.js';
 import MeditationScreen from './radiant-backend/app/screens/MeditationScreen.js';
+import ProfileScreen from './radiant-backend/app/screens/ProfileScreen.js';
 import MeditationPlayerScreen from './radiant-backend/app/screens/MeditationPlayerScreen.js';
 import  JournalScreen  from './radiant-backend/app/screens/JournalScreen.js';
 import MoodTrackerScreen from './radiant-backend/app/screens/MoodTrackerScreen.js';
+import TalkToProScreen from './radiant-backend/app/screens/TalkToProScreen.js';
 import FitnessTrackerScreen from './radiant-backend/app/screens/FitnessTrackerScreen.js';
 
 const Stack = createNativeStackNavigator();
@@ -22,13 +25,15 @@ const App = () => {
         <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="ChangePassword" component={ChangePasswordScreen} />
-          <Stack.Screen name="Register" component={SignupScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
           <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
           <Stack.Screen name="Meditation" component={MeditationScreen} />
           <Stack.Screen name="MeditationPlayer" component={MeditationPlayerScreen} />
           <Stack.Screen name="MoodTracker" component={MoodTrackerScreen} />
           <Stack.Screen name="FitnessTracker" component={FitnessTrackerScreen} />
           <Stack.Screen name="Journal" component={JournalScreen} />
+          <Stack.Screen name="TalkToPro" component={TalkToProScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>);
